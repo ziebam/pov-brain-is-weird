9548d69a8d4f49779b4b757d5558ef7aebd68113
ziebam <ziebamichal@tutanota.com>
2024-02-13 19:22:07 +0100

    Format `nob.c`

--- nob.c
+++ nob.c
@@ -12,25 +12,30 @@ static const char *raylib_modules[] = {
     "utils",
 };
 
-bool build_raylib() {
+bool build_raylib()
+{
     bool result = true;
 
     Nob_Cmd cmd = {0};
     Nob_File_Paths object_files = {0};
     Nob_Procs procs = {0};
 
-    if (!nob_mkdir_if_not_exists("./build/raylib")) nob_return_defer(false);
+    if (!nob_mkdir_if_not_exists("./build/raylib"))
+        nob_return_defer(false);
 
     const char *build_path = "./build/raylib/gcc";
-    if (!nob_mkdir_if_not_exists(build_path)) nob_return_defer(false);
+    if (!nob_mkdir_if_not_exists(build_path))
+        nob_return_defer(false);
 
-    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i) {
+    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i)
+    {
         const char *input_path = nob_temp_sprintf("./raylib/raylib-5.0/src/%s.c", raylib_modules[i]);
         const char *output_path = nob_temp_sprintf("%s/%s.o", build_path, raylib_modules[i]);
 
         nob_da_append(&object_files, output_path);
 
-        if (nob_needs_rebuild(output_path, &input_path, 1)) {
+        if (nob_needs_rebuild(output_path, &input_path, 1))
+        {
             cmd.count = 0;
             nob_cmd_append(&cmd, "gcc");
             nob_cmd_append(&cmd, "-DPLATFORM_DESKTOP", "-fPIC");
@@ -42,12 +47,14 @@ bool build_raylib() {
             nob_da_append(&procs, proc);
         }
     }
-    if (!nob_procs_wait(procs)) nob_return_defer(false);
+    if (!nob_procs_wait(procs))
+        nob_return_defer(false);
 
     cmd.count = 0;
     Nob_String_Builder input_path = {0};
     nob_cmd_append(&cmd, "ar", "-crs", "./build/raylib/gcc/libraylib.a");
-    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i) {
+    for (size_t i = 0; i < NOB_ARRAY_LEN(raylib_modules); ++i)
+    {
         input_path.count = 0;
         nob_sb_append_cstr(&input_path, "./build/raylib/gcc/");
         nob_sb_append_cstr(&input_path, raylib_modules[i]);
@@ -55,7 +62,8 @@ bool build_raylib() {
         nob_sb_append_null(&input_path);
         nob_cmd_append(&cmd, nob_temp_strdup(input_path.items));
     }
-    if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
+    if (!nob_cmd_run_sync(cmd))
+        nob_return_defer(false);
 
 defer:
     nob_cmd_free(cmd);
@@ -65,7 +73,8 @@ defer:
     return result;
 }
 
-bool build_pov_brain_is_weird() {
+bool build_pov_brain_is_weird()
+{
     bool result = true;
 
     Nob_Cmd cmd = {0};
@@ -81,20 +90,25 @@ bool build_pov_brain_is_weird() {
     nob_cmd_append(&cmd, "-lwinmm", "-lgdi32");
     nob_cmd_append(&cmd, "-static");
 
-    if (!nob_cmd_run_sync(cmd)) nob_return_defer(false);
+    if (!nob_cmd_run_sync(cmd))
+        nob_return_defer(false);
 
 defer:
     nob_cmd_free(cmd);
     return result;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     NOB_GO_REBUILD_URSELF(argc, argv);
 
-    if (!nob_mkdir_if_not_exists("build")) return 1;
-    
-    if (!build_raylib()) return 1;
-    if (!build_pov_brain_is_weird()) return 1;
+    if (!nob_mkdir_if_not_exists("build"))
+        return 1;
+
+    if (!build_raylib())
+        return 1;
+    if (!build_pov_brain_is_weird())
+        return 1;
 
     return 0;
 }
